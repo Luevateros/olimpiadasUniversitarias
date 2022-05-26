bd14d77b54a2828947d4a3a01806f43f3a691f94
xKary <karyxprado@ciencias.unam.mx>
2022-05-26 01:28:45 -0500

    Disciplina CRD

---
+++ olimpiadas/src/main/java/com/shrek/olimpiadas/controlador/CtrDisciplina.java
@@ -0,0 +1,82 @@
+package com.shrek.olimpiadas.controlador;
+
+import com.shrek.olimpiadas.dto.CompetidorDTO;
+import com.shrek.olimpiadas.dto.DisciplinaDTO;
+import com.shrek.olimpiadas.dto.EntrenadorDTO;
+import com.shrek.olimpiadas.modelo.Disciplina;
+import com.shrek.olimpiadas.servicio.SvcDisciplina;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class CtrDisciplina {
+    @Autowired
+    private SvcDisciplina svc;
+
+    @GetMapping("/disciplinas")
+    public String mostrarFormNueva(Model model){
+        model.addAttribute("disciplinas", svc.mostrarDisciplinas());
+        return "ver_disciplinas";
+    }
+
+    @RequestMapping("/disciplina/nueva")
+    public String mostrarFormaNueva(Model model) {
+        model.addAttribute("tituloPagina", "Registrar disciplina");
+        model.addAttribute("disciplina", new DisciplinaDTO());
+        return "registro_disciplina";
+    }
+
+    @PostMapping("/disciplinas/guardar")
+    public String agregarDisciplina(DisciplinaDTO disciplina, RedirectAttributes ra, Model model) {
+        model.addAttribute("mensaje", "Registro completo.");
+        String respuesta = svc.agregarDisciplina(disciplina);
+        if (respuesta == null){
+            ra.addFlashAttribute("mensaje", "La disciplina se agregó con exitó.");
+            return "redirect:/disciplinas";
+        }
+        ra.addFlashAttribute("disciplina", new DisciplinaDTO());
+        ra.addFlashAttribute("tituloPagina", "Registrar disciplina");
+        ra.addFlashAttribute("mensaje", respuesta);
+        return "redirect:/registro_disciplina";
+    }
+
+    @GetMapping("/disciplina/editar/{id}")
+    public String actualizarDisciplina(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes ra) {
+        DisciplinaDTO disciplina = svc.mostrarDisciplina(id);
+        if(disciplina == null) {
+            ra.addFlashAttribute("mensaje", "No se encontró la disciplina.");
+            return "redirect:/disciplinas";
+        }
+        model.addAttribute("disciplina", disciplina);
+        model.addAttribute("tituloPagina", "Editar disciplina");
+        return "registro_disciplina";
+    }
+
+    @PostMapping("/disciplinas/actualizar/{id}")
+    public void actDisciplina(DisciplinaDTO disciplina, RedirectAttributes ra, Model model){
+        System.out.print(disciplina);
+    }
+
+
+    @GetMapping("/disciplinas/eliminar/{id}")
+    public String eliminarDisciplina(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes ra) {
+        DisciplinaDTO disciplina = svc.mostrarDisciplina(id);
+        if(disciplina== null) {
+            ra.addFlashAttribute("mensaje", "No se encontró la disciplina");
+            return "redirect:/disciplinas";
+        }
+        svc.eliminarDisciplina(id);
+        ra.addFlashAttribute("mensaje", "La discilina se eliminó con exitó.");
+        return "redirect:/disciplinas";
+    }
+
+}
---
+++ olimpiadas/src/main/java/com/shrek/olimpiadas/dto/DisciplinaDTO.java
@@ -0,0 +1,89 @@
+package com.shrek.olimpiadas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class DisciplinaDTO {
+    private Integer iddisciplina;
+    private String nombre;
+    private String responsable;
+    private String descripcion;
+    private String imagen;
+    /**
+     * Método que regresa el id de la discilina.
+     * @return Integer - Identificador disciplina
+     * */
+    public Integer getIddisciplina() {
+        return iddisciplina;
+    }
+
+    /**
+     * Método que regresa el nombre de la discilina.
+     * @return String - Nombre disciplina
+     * */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Método que establece el nombre de la discilina.
+     * @param nombre - Nombre para la disciplina
+     * */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Método que regresa el representante de la discilina.
+     * @return String - Nombre del representante de la disciplina
+     * */
+    public String getResponsable() {
+        return responsable;
+    }
+
+    /**
+     * Método que establece el representatnte de la discilina.
+     * @param responsable - Nombre del responsable
+     * */
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    /**
+     * Método que regresa la descripción de la discilina.
+     * @return String - Descripción de la disciplina
+     * */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * Método que establece la descripción de la discilina.
+     * @param descripcion- Descripción de la disciplina
+     * */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * Método que regresa el nombre de la imagen de la discilina.
+     * @return String - Nombre de la imagen de la disciplina
+     * */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * Método que establece la descripción de la discilina.
+     * @param imagen - Nombre de la imagen de la disciplina
+     * */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public  String toString(){
+        return getIddisciplina() + " " + getNombre();
+    }
+}
